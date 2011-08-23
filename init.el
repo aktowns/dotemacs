8b45bab04d34aeb25eae2a4375fb4682fd7f1b47
Ashley Towns <ashleyis@me.com>
2011-08-23 19:01:00 +1000

    updated init.el

--- init.el
+++ init.el
@@ -4,6 +4,10 @@
 ; add /usr/local/bin to exec-path (brew etc)
 (push "/usr/local/bin" exec-path)
 
+; quick edit init
+(defun ein () 
+  (find-file "~/.emacs.d/init.el"))
+
 ; setup ELPA repos
 (setq package-archives '(("ELPA" . "http://tromey.com/elpa/") 
 			 ("gnu" . "http://elpa.gnu.org/packages/")
@@ -11,7 +15,18 @@
 
 (require 'cl)
 
+(defvar *emacs-load-start* (current-time))
+
 (add-to-list 'load-path "~/.emacs.d/")
 (add-to-list 'load-path "~/.emacs.d/customisations")
-(add-to-list 'load-path "~/.emacs.d/utils")
+(add-to-list 'load-path "~/.emacs.d/themes")
+(add-to-list 'load-path "~/.emacs.d/utilities")
+(add-to-list 'load-path "~/.emacs.d/vendor")
 
+(load "~/.emacs.d/load-directory.el")
+(mapcar 'load-directory '("~/.emacs.d/utilities"))
+
+(mapcar 'load-directory '("~/.emacs.d/customisations"))
+
+(message "My .emacs loaded in %ds" (destructuring-bind (hi lo ms) (current-time)
+				     (- (+ hi lo) (+ (first *emacs-load-start*) (second *emacs-load-start*)))))
